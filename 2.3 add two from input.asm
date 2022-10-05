eb2b48bc69e9d3707486a2274cc59e2f6e4cdeac
Tom <git@tomr.me>
2022-10-05 11:44:57 +0100

    what huh why didn't these get deleted

--- 2.3 add two from input.asm
+++
@@ -1,7 +0,0 @@
-; ADD TWO INPUTS
-INP		;Request input from user, which will be loaded into acc (901)
-STO	99	;Store content of acc (user input) at address 99 (342)
-INP		;Request input from user, loaded into acc (901)
-ADD	99	;Add the content of memory address 99 to the acc (199)
-OUT		;Output the content of the acc (902)
-HLT		;000
--- 2.4.3 calc difference.asm
+++
@@ -1,8 +0,0 @@
-; CALC DIFF BETWEEN TWO INPUTS
-
-INP		;user input to acc
-STO 90	;store acc (user input) to 90
-INP		;input to acc
-SUB 90	;sub # at 90 (first input) from acc (last input)
-OUT
-HLT
